bd266ce8c9f4882fcd569f682c1efeb0be430a1f
enjoycompany <enjoycompany@gmail.com>
2013-06-25 16:10:59 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Welcome to the TVWC Office Bearers Blog
+This file will eventually contain instructions for using this blog
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OBBlog::Application.config.secret_token = '5755f0509170a1f82abb1de39ef8467705017a6d1ddeff99cb2eb782ad332582c438d3e2e139e9e074072e3a16f2293ab64be0b90a2a535a734252c415aacb49'
